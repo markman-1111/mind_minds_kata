b64d1ce2f9437b39b756ddcfcc8550d84b9db0d0
markman-1111 <captain_tripps@hotmail.com>
2017-07-05 14:02:10 -0400

    Add files via upload

--- kata_test.rb
+++ kata_test.rb
@@ -1,8 +1,11 @@
 require "minitest/autorun"
-require_relative "div.rb"
+require_relative "Kata_function.rb"
 
 class Kata_test < Minitest::Test
 	def test_1_1
 		assert_equal(1,1)
+	end
+	def test_100
+		double(100)
 	end
 end
